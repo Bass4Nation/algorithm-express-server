96fb223b85084a785ba3d94f64d83b19eda95d3b
Kristoffer Snopestad Søderkvist <bass4nation@gmail.com>
2023-04-15 16:13:31 +0200

    Working on bubblesort

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,10 +1,77 @@
 import request from 'supertest';
 import app from './app';
 
+// -----------------  Hello World and more letters and words and stuff :) -----------------
 describe('GET /', () => {
   it('responds with "Hello World!"', async () => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
-    expect(response.text).toBe('Hello World!');
+    expect(response.text).toBe('Hello World! From Express Server in Typescript with Jest Testing Framework and Supertest for testing HTTP requests and responses in Express Server in Typescript!');
   });
 });
+
+//  -----------------  Sort Numbers High to Low  -----------------
+describe('GET /sort_h_t_l', () => {
+  it('responds with numbers high to low', async () => {
+    const command = '/sort_h_t_l';
+
+    const numbers: number[] = [1, 2, 3, 4, 5, 2];
+    const stringNumbers = numbers.join(',');
+    const dataString = '?numbers=' + stringNumbers;
+
+    const collectedCommand =  command + dataString;
+    const response = await request(app).get(collectedCommand);
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toBe('5,4,3,2,2,1');
+  });
+});
+
+//  -----------------  Top 2 Highest  -----------------
+describe('GET /top2highest', () => {
+  it('should return 2 highest number', async () => {
+    const command = '/top2highest';
+
+    const response = await request(app).get(command);
+    expect(response.statusCode).toBe(200);
+  });
+});
+
+// -----------------  Bubble Sort  -----------------
+describe('GET /bubble_sort', () => {
+  it('should return bubble sorted numbers', async () => {
+    const command = '/bubble_sort';
+
+    const response = await request(app).get(command);
+    expect(response.statusCode).toBe(200);
+  });
+});
+
+//  -----------------  Firebase  -----------------
+// describe('GET /firebase', () => {
+//   it('should return firebase data', async () => {
+//     const command = '/firebase';
+
+//     const response = await request(app).get(command);
+//     expect(response.statusCode).toBe(200);
+//   });
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+// describe('GET /', () => {
+//   it('responds with "Hello World!"', async () => {
+//     const response = await request(app).get('/');
+//     expect(response.statusCode).toBe(200);
+//     expect(response.text).toBe('Hello World! From Express Server in Typescript with Jest Testing Framework and Supertest for testing HTTP requests and responses in Express Server in Typescript!');
+//   });
+// });
--- src/app.ts
+++ src/app.ts
@@ -4,29 +4,27 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World! From Express Server in Typescript with Jest Testing Framework and Supertest for testing HTTP requests and responses in Express Server in Typescript! ');
+  // console.log('You sent a GET request to the / endpoint');
+  res.send('Hello World! From Express Server in Typescript with Jest Testing Framework and Supertest for testing HTTP requests and responses in Express Server in Typescript!');
 });
 
-app.get('/sort', (req: Request, res: Response) => {
+// Sort numbers high to low
+app.get('/sort_h_t_l', (req: Request, res: Response) => {
   const numbersParam = req.query.numbers;
 
   if (typeof numbersParam === 'string') {
     const numbers = numbersParam.split(',').map(Number);
 
-    numbers.forEach(number => {
-      console.log(number);
-    });
+    // numbers.forEach(number => {
+    //   console.log(number);
+    // });
 
     const sortedNumbers = sortNumbersHighToLow(numbers);
     const sortedNumbersString = convertNumbersToString(sortedNumbers);
-    console.log(sortedNumbersString);
-    
-
-
-    
+    // console.log(sortedNumbersString);
 
     // You can also send a response to the user after processing the numbers
-    res.send('Received and processed the numbers' + sortedNumbersString );
+    res.send(sortedNumbersString);
   } else {
     res.status(400).send('Invalid input');
   }
@@ -36,6 +34,69 @@ const sortNumbersHighToLow = (numbers: number[]) => {
   return numbers.sort((a, b) => b - a);
 };
 
+app.get('/top2highest', (req: Request, res: Response) => {
+  // const numbersParam = req.query.numbers;
+  const testNumbers = [1, 2, 3, 4, 5, 2, 10, 999];
+  const sortedNumbers = sortNumbersHighToLow(testNumbers);
+  const top2HighestNumbers = sortedNumbers.slice(0, 2);
+  // console.log(top2HighestNumbers);
+  res.status(200).send(top2HighestNumbers);
+});
+
+// -----------------  Bubble Sort  -----------------
+// Not working yet - need to fix 
+app.get('/bubble_sort', (req: Request, res: Response) => {
+  // const numbersParam = req.query.numbers;
+  const testNumbers: number[] = [6,3,0,5]; // [0,3,5,6]
+  const numbersDelivered: number[] = testNumbers;
+  const displayCurrentNumbers: number[] = [];
+  const sortedNumbers: number[] = [];
+  
+  // for (let i = 0; i < testNumbers.length; i++) {
+  //   for (let j = 0; j < testNumbers.length; j++) {
+  //     if (testNumbers[j] > testNumbers[j + 1]) {
+  //       const temp = testNumbers[j];
+  //       console.log('temp: ', temp);
+  //       testNumbers[j] = testNumbers[j + 1];
+  //       testNumbers[j + 1] = temp;
+  //     }
+  //   }
+  // }
+
+  
+  // try {
+  //   let index = 0;
+  //   let currentNumber = numbersDelivered[index];
+
+  //   testNumbers.forEach(number => {
+  //     if (currentNumber > number) {
+  //       displayCurrentNumbers.push(number);
+  //       displayCurrentNumbers.push(currentNumber);
+  //       currentNumber = number;
+  //     } else {
+  //       displayCurrentNumbers.push(currentNumber);
+  //       displayCurrentNumbers.push(number);
+  //       currentNumber = number;
+  //     }
+  //     index++;
+  //     console.log('displayCurrentNumbers: ', displayCurrentNumbers);
+      
+  //   });
+
+  // } catch (error) {
+  //   console.error(error);
+  // }
+
+
+  // console.log(testNumbers);
+
+
+  res.status(200).send('Bubble sort not implemented yet');
+});
+
+
+
+// Used to convert numbers to string for the response to the user
 const convertNumbersToString = (numbers: number[]) => {
   return numbers.join(',');
 };
