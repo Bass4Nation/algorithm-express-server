0ede7fb6a3ebb62ea808c4820a26d96e76c1f279
Kristoffer Snopestad Søderkvist <bass4nation@gmail.com>
2023-04-16 13:12:01 +0200

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -1,7 +1,9 @@
 // firebase.ts
 
 import * as admin from 'firebase-admin';
-import * as serviceAccount from './portfolio-express-e4e7e-firebase-adminsdk-w21eg-dc23347a5a.json';
+// import * as serviceAccount from './portfolio-express-e4e7e-firebase-adminsdk-w21eg-dc23347a5a.json';
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY || '');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
